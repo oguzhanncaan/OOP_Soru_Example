960eae55dd1be1c20e5c3f55dbbd11117840d7ba
oguzhanncaan <oguzhancan.tr@outlook.com>
2022-02-06 13:15:01 +0300

    Initial commit

---
+++ src/com/oguzhancan/Runner.java
@@ -0,0 +1,18 @@
+package com.oguzhancan;
+
+public class Runner {
+    public static void main(String[] args) {
+        /**
+         * SORU:
+         * Bir firma ürünlerini tutacak, 3 grup ürünü var.
+         * BireyselUrun, KurumsalUrun, OzelYapimUrun  --- > ürün sınıfı
+         * id,ad,marka,model,fiyat,stokMiktari,uyarimMiktari alanları ortaktır. ---> kalıtım, get-set- constructorlar
+         * Tüm ürünler için tek bir satisRepository sınıfı vardır. Burada ürüne göre satış yapılıyor. 
+         * Yukarıda ki bilgilere göre;
+         * 1-Kalıtım yapınız.
+         * 2-Her sınıfta, default ve parametreli constructorlar yazınız.
+         * 3-Bazı özelliklerin ve methodların public, private ve protected olması gerekli.
+         * 4-Runner da nesne oluşturup, Repository sınıfında satışını yapınız.
+         */
+    }
+}
